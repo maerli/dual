c11c8b2f3645192b3f3c617ad8edb1c5fe9240ac
maerli <maerli.pereira@gmail.com>
2020-07-12 13:57:09 -0300

    adding new_df function

--- diff/revediff/__init__.py
+++ diff/revediff/__init__.py
@@ -23,6 +23,20 @@ def df(f,*args):
 	if len(ans) == 1: 
 		return ans[0]
 	return tuple(ans)
+#testing df
+def new_df(*args):
+	tam = len(args)
+	if tam == 1:
+		def _ok(*_args):
+			 y = args[0](*_args)
+			 y.grad_value = 1.0
+			 ans = [x.grad() for x in _args if type(x) is Var]
+			 tans = tuple(ans)
+			 return (y.value, *tans)
+		return _ok
+	elif tam > 1:
+		f,*arx = args
+		return df(f,*arx)
 def const(x):
 	if type(x) is Var:
 		pass
